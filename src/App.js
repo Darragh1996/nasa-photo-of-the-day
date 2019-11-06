b6cc8227e55e918a69f071b2f1b6fd288b01bc98
Darragh Ferry <darraghferry@hotmail.com>
2019-11-06 19:51:57 +0000

    fixed some issues

--- src/App.js
+++ src/App.js
@@ -2,33 +2,41 @@
 
 import React, { useEffect, useState } from "react";
 import "./App.css";
-import axios from 'axios';
-import { Card } from './components/Card';
+import axios from "axios";
+import { Card } from "./components/Card";
 
 function App() {
+  const [nasaInfo, setNasaInfo] = useState([]);
 
-const [nasaInfo, setNasaInfo] = useState({});
-
-  useEffect(() =>{
-    axios.get('https://api.nasa.gov/planetary/apod?api_key=1F5F9mMau9iKruv7BeHJOWu1C34yR20vUAbzJYiN')
-      .then((res) =>{
+  useEffect(() => {
+    axios
+      .get(
+        "https://api.nasa.gov/planetary/apod?api_key=1F5F9mMau9iKruv7BeHJOWu1C34yR20vUAbzJYiN"
+      )
+      .then(res => {
         setNasaInfo(res.data);
         console.log(res.data);
       })
-      .catch(err =>{
+      .catch(err => {
         console.log(err);
-      })
+      });
 
-      return console.log('cleanup')
-
+    return console.log("cleanup");
   }, []);
 
   return (
     <div className="App">
-      {nasaInfo
-      ? <Card image={nasaInfo.url} title={nasaInfo.title} explanation={nasaInfo.explanation} date={nasaInfo.date} hdurl={nasaInfo.hdurl}/>
-      : 'No Data'
-      }
+      {nasaInfo ? (
+        <Card
+          image={nasaInfo.url}
+          title={nasaInfo.title}
+          explanation={nasaInfo.explanation}
+          date={nasaInfo.date}
+          hdurl={nasaInfo.hdurl}
+        />
+      ) : (
+        "No Data"
+      )}
     </div>
   );
 }
